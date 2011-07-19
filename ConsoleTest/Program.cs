1658192df35a5732dfde7618e968c4bda7f99104
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2011-07-19 05:22:40 +0000

    Fantasy.Base [1.0.0.38] -- Add ObjectBuilder for alwasy creating new object in addin

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fantasy.AddIns;
+using System.Xml;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AddInParser parser = new AddInParser();
+            //XmlReader reader = XmlReader.Create("test.addin.xaml");
+            //AddIn addIn = parser.Parse(reader);
+            //reader.Close();
+
+            DefaultAddInTree.Initialize(new string[] { "test.addin.xaml" });
+            object[] rs = AddInTree.Tree.GetTreeNode("fantasy/services").BuildChildItems<object>(null).ToArray();
+
+        }
+    }
+}
---
+++ Fantasy.Base/AddIns/ObjectBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xaml;
+
+
+namespace Fantasy.AddIns
+{
+    [System.Windows.Markup.ContentProperty("Template")]
+    public class ObjectBuilder
+    {
+        public ObjectBuilder()
+        {
+            CurrentInstance = this;
+        }
+        public object Build()
+        {
+            XamlReader xr = this.XamlNodes.GetReader();
+            XamlObjectWriter xow = new XamlObjectWriter(xr.SchemaContext);
+            while (xr.Read())
+            {
+                xow.WriteNode(xr);
+            }
+            xr.Close();
+            return xow.Result;
+        }
+
+        public object Template { get; set; }
+
+        internal XamlNodeList XamlNodes { get; set; }
+
+        [ThreadStatic]
+        internal static ObjectBuilder CurrentInstance;
+    }
+}
