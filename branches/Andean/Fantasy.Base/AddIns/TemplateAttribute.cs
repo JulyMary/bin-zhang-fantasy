76e10fb14afea7678f8f90e84cdd7989d0fbc47d
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2011-07-21 05:45:20 +0000

    ClickView.Base [1.0.0.40] -- Use TemplateAttribute to assign objectbuilder

---
+++ branches/Andean/Fantasy.Base/AddIns/TemplateAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fantasy.AddIns
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class TemplateAttribute : Attribute 
+    {
+        public TemplateAttribute(string buildMember)
+        {
+            BuildMember = buildMember;
+        }
+
+        public string BuildMember { get; private set; }
+    }
+}
