872a13d8d178a68dccedff9d17f23020060bf5ce
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2011-08-18 07:35:08 +0000

    Fantasy.Studio.BusinessEngine [1.0.0.18] -- Add NodeExpander for business class shape

---
+++ Fantasy.Studio.BusinessEngine/ClassDiagramEditing/Shapes/NodeExpander.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace Fantasy.Studio.BusinessEngine.ClassDiagramEditing.Shapes
+{
+    public class NodeExpander : Expander
+    {
+        static NodeExpander()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NodeExpander), new FrameworkPropertyMetadata(typeof(NodeExpander)));
+
+        }
+
+        public NodeExpander()
+        {
+
+        }
+    }
+}
