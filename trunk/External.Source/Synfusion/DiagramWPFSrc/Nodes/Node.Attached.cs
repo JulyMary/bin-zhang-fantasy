032800cafffc3523a9adde373fc01eb663150024
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2011-08-30 07:41:31 +0000

    Fantasy.Base [1.0.0.23] -- Add BooleanInverterConverter Fantasy.BusinessEngine [1.0.0.8] -- Set package-class mapping cascade to none Fantasy.Studio [1.0.0.18] -- Add SelectionType condition Fantasy.Studio.BusinessEngine [1.0.0.23] -- Adding delete property, delete class symbol on classdiagram

---
+++ trunk/External.Source/Synfusion/DiagramWPFSrc/Nodes/Node.Attached.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Syncfusion.Windows.Diagram
+{
+    partial class Node
+    {
+
+        public static bool GetIsSelectionScope(UIElement obj)
+        {
+            return (bool)obj.GetValue(IsSelectionScopeProperty);
+        }
+
+        public static void SetIsSelectionScope(UIElement obj, bool value)
+        {
+            obj.SetValue(IsSelectionScopeProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for IsSelectionScope.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsSelectionScopeProperty =
+            DependencyProperty.RegisterAttached("IsSelectionScope", typeof(bool), typeof(Node), new UIPropertyMetadata(false));
+
+        public static bool IsInSelectionScope(Node node, DependencyObject child)
+        {
+            bool rs = false;
+            while (child != null && ! object.Equals(node, child) && !rs)
+            {
+                UIElement ui = child as UIElement;
+                if (ui != null)
+                {
+                    rs = Node.GetIsSelectionScope(ui);
+                }
+
+                if (child is Visual || child is System.Windows.Media.Media3D.Visual3D)
+                {
+                    child = VisualTreeHelper.GetParent(child);
+                }
+                else
+                {
+                    child = LogicalTreeHelper.GetParent(child);
+                }
+            }
+
+            return rs;
+
+        }
+
+
+    }
+}
