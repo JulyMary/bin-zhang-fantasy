6f7ddf7889b4ea622ecd3e7b9d091f7996c1c77c
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2011-09-07 07:15:11 +0000

    Fantasy [1.0.0.66] -- Add reference from GAC

---
+++ branches/Andean/Fantasy.Studio.BusinessEngine/AssemblyReference/AddGACReferenceCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fantasy.ServiceModel;
+using Fantasy.BusinessEngine.Services;
+using System.Reflection;
+using Fantasy.BusinessEngine;
+using System.Windows.Threading;
+
+namespace Fantasy.Studio.BusinessEngine.AssemblyReference
+{
+    public class AddGACReferenceCommand : ObjectWithSite, ICommand
+    {
+        public object Execute(object args)
+        {
+
+            AddGACReferenceDialog dlg = new AddGACReferenceDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                IEntityService es = this.Site.GetRequiredService<IEntityService>();
+                BusinessAssemblyReferenceGroup group = es.GetAssemblyReferenceGroup();
+                es.DefaultSession.BeginUpdate();
+                try
+                {
+                    foreach (Assembly assembly in dlg.SelectedAssemblies)
+                    {
+
+                        string name = assembly.GetName().Name;
+                        if (!group.References.Any(r => string.Equals(name, r.Name, StringComparison.OrdinalIgnoreCase)))
+                        {
+                            BusinessAssemblyReference reference = es.CreateEntity<BusinessAssemblyReference>();
+                            reference.FullName = assembly.FullName;
+                            reference.Group = group;
+                            group.References.Add(reference);
+                            reference.CopyLocal = false;
+                            es.DefaultSession.SaveOrUpdate(reference);
+                        }
+                    }
+                    es.DefaultSession.EndUpdate(true);
+                }
+                catch
+                {
+                    es.DefaultSession.EndUpdate(false);
+                    throw;
+                }
+
+            }
+
+            return null;
+        }
+
+
+    }
+}
---
+++ branches/Andean/Fantasy.Studio.BusinessEngine/AssemblyReference/AssemblyVersionConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Reflection;
+
+namespace Fantasy.Studio.BusinessEngine.AssemblyReference
+{
+    public class AssemblyVersionConverter : IValueConverter
+    {
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Assembly assembly = (Assembly)value;
+            return assembly.GetName().Version;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
