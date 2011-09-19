784dfd58b95751cb8223bde513f91061a88c39bc
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2011-09-19 10:58:21 +0000

    Fantasy [1.0.0.85] -- Added adding users to role panel

---
+++ Fantasy.Studio.BusinessEngine/UserRoleEditing/BusinessUserPicker.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Fantasy.BusinessEngine;
+using Fantasy.Studio.Controls;
+
+namespace Fantasy.Studio.BusinessEngine.UserRoleEditing
+{
+    /// <summary>
+    /// Interaction logic for BusinessRolePicker.xaml
+    /// </summary>
+    public partial class BusinessUserPicker : Window
+    {
+        public BusinessUserPicker()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void extendableTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            ITreeViewItem item = e.NewValue as ITreeViewItem;
+            BusinessUser entity = item != null ? item.DataContext as BusinessUser : null;
+            ((BusinessUserPikcerModel)this.DataContext).SelectedItem = entity;
+        }
+
+        private void extendableTreeView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            BusinessRolePikcerModel model = (BusinessRolePikcerModel)this.DataContext;
+            if (model.SelectedItem != null)
+            {
+                this.DialogResult = true;
+            }
+        }
+    }
+}
--- Fantasy.Studio.BusinessEngine/UserRoleEditing/DragBusinessUserDropHandler.cs
+++ Fantasy.Studio.BusinessEngine/UserRoleEditing/DragBusinessUserDropHandler.cs
@@ -8,22 +8,30 @@ using Fantasy.Windows;
 
 namespace Fantasy.Studio.BusinessEngine.UserRoleEditing
 {
-    public class DragBusinessRoleOverHandler : ObjectWithSite, IEventHandler<DragEventArgs>
+    public class DragBusinessUserDropHandler : ObjectWithSite, IEventHandler<DragEventArgs>
     {
-
         #region IEventHandler<DragEventArgs> Members
 
         public void HandleEvent(object sender, DragEventArgs e)
         {
-            BusinessRole data = e.Data.GetDataByType<BusinessRole>();
-            if (data != null && ! data.IsComputed)
+
+            BusinessUser user = e.Data.GetDataByType<BusinessUser>();
+            if (user != null)
             {
                 e.Effects = DragDropEffects.Copy;
                 e.Handled = true;
+
+                BusinessRole role = this.Site.GetRequiredService<BusinessRole>();
+
+                if (role.Users.IndexOf(user) < 0)
+                {
+                    role.Users.Add(user);
+                    user.Roles.Add(role);
+                   
+                }
             }
         }
 
         #endregion
     }
-   
 }
