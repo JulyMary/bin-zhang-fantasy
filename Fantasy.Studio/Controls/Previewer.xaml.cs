c725d60a88120db900f1c761205fd9af9e1985a7
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2011-10-04 13:50:19 +0000

    Fantasy [1.0.0.111] -- Added Preview Viewer on class diagram

--- Fantasy.Studio/Controls/Previewer.xaml.cs
+++ Fantasy.Studio/Controls/Previewer.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Diagnostics;
 
 namespace Fantasy.Studio.Controls
 {
@@ -25,6 +27,7 @@ namespace Fantasy.Studio.Controls
         }
 
 
+       
 
         public ScrollViewer View
         {
@@ -43,17 +46,23 @@ namespace Fantasy.Studio.Controls
             }
         }
 
+
+
         private void BindingView(ScrollViewer value)
         {
             if (this.ActualWidth > 0 && this.ActualHeight > 0)
             {
+                this.View.ScrollChanged += new ScrollChangedEventHandler(View_ScrollChanged);
+
+              
+
                 ResetScale();
 
 
                 //Set viewPortThumb Layout
                 SetThumbLocation();
 
-                this.View.ScrollChanged += new ScrollChangedEventHandler(View_ScrollChanged);
+                
             }
            
            
@@ -61,12 +70,16 @@ namespace Fantasy.Studio.Controls
 
         }
 
+
+
         private void SetThumbLocation()
         {
-            this.ViewPortThumb.Width = this.PreviewRect.Width * this._scale;
-            this.ViewPortThumb.Height = this.PreviewRect.Height * this._scale;
+            this.ViewPortThumb.Width = this.View.ViewportWidth * this._scale;
+            this.ViewPortThumb.Height = this.View.ViewportHeight * this._scale;
             double vptLeft = this.View.HorizontalOffset * _scale + this._previewLeft;
             double vptTop = this.View.VerticalOffset * _scale + this._previewTop;
+
+            
             Canvas.SetLeft(this.ViewPortThumb, vptLeft);
             Canvas.SetTop(this.ViewPortThumb, vptTop);
         }
@@ -75,33 +88,28 @@ namespace Fantasy.Studio.Controls
         {
             double scaleX = this.ActualWidth / this.View.ExtentWidth;
             double scaleY = this.ActualHeight / this.View.ExtentHeight;
-            if (scaleX < scaleY)
-            {
-                this.PreviewRect.Width = this.ActualWidth;
-                this.PreviewRect.Height = this.ActualWidth * this.View.ExtentHeight / this.View.ExtentWidth;
-                this._scale = scaleX; 
-            }
-            else
-            {
-                this.PreviewRect.Height = this.ActualHeight;
-                this.PreviewRect.Height = this.ActualHeight * this.View.ExtentWidth / this.View.ExtentHeight ;
-                this._scale = scaleY; 
-            }
+            this._scale = Math.Min(scaleX, scaleY);
+
+            this.PreviewBorder.Width = this.View.ExtentWidth * this._scale;
+            this.PreviewBorder.Height = this.View.ExtentHeight * this._scale;
+           
+            
 
 
             //Set PreviewRect Layout;
-           
-            this._previewLeft = this.ActualWidth - this.PreviewRect.Width / 2;
-            this._previewTop = this.ActualHeight - this.PreviewRect.Height / 2;
-            Canvas.SetLeft(this.PreviewRect, this._previewLeft);
-            Canvas.SetTop(this.PreviewRect, this._previewTop);
+
+            this._previewLeft = (this.ActualWidth - this.PreviewBorder.Width) / 2;
+            this._previewTop = (this.ActualHeight - this.PreviewBorder.Height) / 2;
+            Canvas.SetLeft(this.PreviewBorder, this._previewLeft);
+            Canvas.SetTop(this.PreviewBorder, this._previewTop);
+
 
             FrameworkElement content = (FrameworkElement)this.View.Content;
-
-            VisualBrush visualBrush = new VisualBrush(this.View);
+            VisualBrush visualBrush = new VisualBrush(content);
             visualBrush.Stretch = Stretch.Uniform;
-            visualBrush.ViewboxUnits = BrushMappingMode.Absolute;
-            visualBrush.Viewbox = new Rect(0, 0, content.ActualWidth, content.ActualHeight);
+            visualBrush.ViewboxUnits = BrushMappingMode.RelativeToBoundingBox;
+            visualBrush.Viewbox = new Rect(0, 0, 1, 1);
+            //visualBrush.Transform = (Transform)content.LayoutTransform.Inverse;
             this.PreviewRect.Fill = visualBrush;
 
             this.SetThumbLocation();
@@ -130,27 +138,26 @@ namespace Fantasy.Studio.Controls
             DependencyProperty.Register("View", typeof(ScrollViewer), typeof(Previewer), new PropertyMetadata(null));
 
 
-        private double _dragStartLeft;
-        private double _dragStartTop;
-
-        private void ViewPortThumb_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
-        {
-            _dragStartLeft = Canvas.GetLeft(this.ViewPortThumb);
-            _dragStartTop = Canvas.GetTop(this.ViewPortThumb);
-        }
+       
 
         private void ViewPortThumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
         {
-            double offSetX = (e.HorizontalChange + _dragStartLeft) / this._scale;
+
+            Debug.WriteLine(e.HorizontalChange + ", " + e.VerticalChange);
+
+            double offSetX = (e.HorizontalChange / this.PreviewBorder.ActualWidth) * this.View.ExtentWidth + this.View.HorizontalOffset; 
             offSetX = Math.Max(0, offSetX);
             offSetX = Math.Min(offSetX, this.View.ExtentWidth - this.View.ViewportWidth);
 
-            double offsetY = (e.VerticalChange + this._dragStartTop) / this._scale;
+            double offsetY = (e.VerticalChange / this.PreviewBorder.ActualHeight) * this.View.ExtentHeight + this.View.VerticalOffset;
             offsetY = Math.Max(0, offsetY);
             offsetY = Math.Min(offsetY, this.View.ExtentHeight - this.View.ViewportHeight);
 
             this.View.ScrollToHorizontalOffset(offSetX);
             this.View.ScrollToVerticalOffset(offsetY);
+
+            
+
         }
 
         private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
