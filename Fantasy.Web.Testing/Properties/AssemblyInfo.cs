affc6623e675d52e49715dda8bf516465ce7b8c0
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2011-11-01 06:31:49 +0000

    Fantasy -- Adding Fantasy.Web infrastructure

--- Fantasy.Web.Testing/Properties/AssemblyInfo.cs
+++ Fantasy.Web.Testing/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fantasy.Web")]
+[assembly: AssemblyTitle("Fantasy.Web.Testing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Fantasy.Web")]
+[assembly: AssemblyProduct("Fantasy.Web.Testing")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("217b1c41-2a54-4221-a397-11dc3feda6e8")]
+[assembly: Guid("87ad904d-23f8-41fc-9337-68d0aa8dfde4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Fantasy.Web/FantasyHttpApplication.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Fantasy.IO;
+using Fantasy.AddIns;
+using Fantasy.ServiceModel;
+
+namespace Fantasy.Web
+{
+    public class FantasyHttpApplication : HttpApplication
+    {
+
+        protected virtual void Application_Start()
+        {
+
+            string dir = LongPath.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+
+            DefaultAddInTree.Initialize(LongPathDirectory.EnumerateAllFiles(dir, "*.addin.xaml"));
+
+            IEnumerable<object> services = AddInTree.Tree.GetTreeNode("fantasy/services").BuildChildItems<object>(null, ServiceManager.Services);
+            ServiceManager.Services.InitializeServices(services.ToArray());
+
+            foreach (ICommand command in AddInTree.Tree.GetTreeNode("fantasy/startup").BuildChildItems<ICommand>(null, ServiceManager.Services))
+            {
+                command.Execute(null);
+            }
+
+           
+            AreaRegistration.RegisterAllAreas();
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected virtual void Application_End()
+        {
+
+        }
+
+        protected virtual void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+           
+        }
+
+        protected virtual void RegisterRoutes(RouteCollection routes)
+        {
+            
+
+        }
+    }
+}
