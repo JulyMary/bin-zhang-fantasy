7af5703640e5901b7b4880ef9dfebf8d32f4f201
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-04-11 13:43:56 +0000

    Adding Security Service

---
+++ branches/Andean/Fantasy.BusinessEngine/Services/SecurityService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fantasy.ServiceModel;
+
+namespace Fantasy.BusinessEngine.Services
+{
+    public class SecurityService : ServiceBase, ISecurityService
+    {
+        #region ISecurityService Members
+
+        public BusinessObjectSecurity GetObjectSecurity(BusinessObject obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BusinessObjectSecurity GetClassSecurity(BusinessClass @class)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
