c852b46f4555369a256d44a1faaa71c809ad39f0
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-04-11 13:43:56 +0000

    Adding Security Service

--- Fantasy.BusinessEngine/BusinessApplication.cs
+++ Fantasy.BusinessEngine/BusinessApplication.cs
@@ -2,14 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Fantasy.BusinessEngine.Services;
 
 namespace Fantasy.BusinessEngine
 {
-    public class BusinessApplication : ObjectWithSite
+    public class BusinessApplication 
     {
        
-
-
         public BusinessApplicationData Data { get; internal set; }
+
+
+        private BusinessObject _entryObject = null;
+
+        public BusinessObject EntryObject
+        {
+            get 
+            {
+                if (this._entryObject == null && this.Data.EntryObjectId != null)
+                {
+                    this._entryObject = BusinessEngineContext.Current.GetRequiredService<IEntityService>().Get<BusinessObject>(this.Data.EntryObjectId);
+                }
+                return _entryObject; 
+            }
+            set 
+            {
+                _entryObject = value;
+                
+            }
+        }
+
+
+        public virtual void Load()
+        {
+
+        }
+
+
+        public virtual void Unload()
+        {
+
+        }
+
+
+        public virtual BusinessObjectSecurity GetObjectSecurity(BusinessObject obj)
+        {
+            return BusinessEngineContext.Current.GetRequiredService<ISecurityService>().GetObjectSecurity(obj);
+        }
+
+
+
+
     }
 }
---
+++ Fantasy.BusinessEngine/Services/ISecurityService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fantasy.BusinessEngine.Services
+{
+    public interface ISecurityService
+    {
+        BusinessObjectSecurity GetObjectSecurity(BusinessObject obj);
+
+        BusinessObjectSecurity GetClassSecurity(BusinessClass @class);
+    }
+}
