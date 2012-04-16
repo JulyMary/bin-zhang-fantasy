ae49e405adf85230f3a6a768e453ea53b4b61ee2
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-04-16 12:21:23 +0000

    Fantasy [1.0.0.147] -- Adding ApplicationViewDesigner

---
+++ branches/Andean/Fantasy.Studio.BusinessEngine/ApplicationEditing/DefaultApplicationViewDesigner.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Fantasy.Web.Design;
+
+namespace Fantasy.Studio.BusinessEngine.ApplicationEditing
+{
+    /// <summary>
+    /// Interaction logic for DefaultApplicationViewDesigner.xaml
+    /// </summary>
+    public partial class DefaultApplicationViewDesigner : UserControl,IObjectWithSite, IViewDesigner
+    {
+        public DefaultApplicationViewDesigner()
+        {
+            InitializeComponent();
+        }
+
+        #region IViewDesigner Members
+
+
+
+
+
+        public void LoadSettings(Fantasy.BusinessEngine.IBusinessEntity entity, System.Xml.Linq.XElement setting)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Xml.Linq.XElement SaveSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UIElement UI
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler SettingChanged;
+
+        #endregion
+
+        #region IObjectWithSite Members
+
+        public IServiceProvider Site
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+    }
+}
