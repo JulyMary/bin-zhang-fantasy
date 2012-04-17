9d0b54cc96da8af57ec0d1500ac40179afddc60a
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-04-17 13:15:04 +0000

    ...................

---
+++ branches/Andean/Fantasy.Studio.BusinessEngine/ApplicationEditing/BusinessApplicationViewDesignPanelModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace Fantasy.Studio.BusinessEngine.ApplicationEditing
+{
+    class BusinessApplicationViewDesignPanelModel
+    {
+
+    }
+
+
+    class WebController
+    {
+        string Description { get; set; }
+
+        ImageSource PreviewImage { get; set; }
+
+        string Caption { get; set; }
+
+        string Type { get; set; }
+    }
+}
