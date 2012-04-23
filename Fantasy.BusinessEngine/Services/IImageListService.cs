0aee9e8f27e6e626c2ff0f5e0defe5b818992f83
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-04-23 12:03:14 +0000

    Fantasy [1.0.0.152] -- Adding ImageListService

---
+++ Fantasy.BusinessEngine/Services/IImageListService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Fantasy.BusinessEngine.Services
+{
+    public interface IImageListService 
+    {
+
+        void Register(string key, byte[] rawImage);
+
+        void Register(string key, Func<byte[]> _imageLoader);
+
+        byte[] GetRawImage(string key);
+
+        bool ContainsImage(string key);
+    }
+}
---
+++ Fantasy.BusinessEngine/Services/ImageListService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fantasy.ServiceModel;
+
+namespace Fantasy.BusinessEngine.Services
+{
+    public class ImageListService : ServiceBase, IImageListService
+    {
+        #region IImageListService Members
+
+        public void Register(string key, byte[] rawImage)
+        {
+            this._images.Add(key, rawImage);
+        }
+
+        public void Register(string key, Func<byte[]> imageLoader)
+        {
+            this._images.Add(key, imageLoader);
+
+            
+        }
+
+        public byte[] GetRawImage(string key)
+        {
+            object img = this._images[key];
+            if(img is byte[])
+            {
+                return (byte[])img;
+            }
+            else
+            {
+                byte[] raw = ((Func<byte[]>)img)(); 
+                this._images[key] = raw;
+                return raw;
+            }
+        
+        }
+
+        public bool ContainsImage(string key)
+        {
+            return this._images.ContainsKey(key);
+        }
+
+        #endregion
+
+
+        Dictionary<string,object> _images = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+       
+    }
+}
