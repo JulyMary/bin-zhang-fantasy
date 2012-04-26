f6275fd6442c236cb6251f7a4f6f89d6db5bca7e
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-04-26 15:10:51 +0000

    Fantasy [1.3.1.11] -- Read default namespaces from web.config file

--- External.Source/razorgenerator/CommonAssemblyInfo.cs
+++ External.Source/razorgenerator/CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.1.1")]
+[assembly: AssemblyVersion("1.3.1.11")]
 [assembly: AssemblyProduct("RazorGenerator")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
