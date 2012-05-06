f3be05180262c8da5aea695cdc05dc92b33c4e49
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-05-06 11:12:02 +0000

    Fantasy -- Adding html.ScalarView method

---
+++ Fantasy.Web/Mvc/Html/BusinessApplicationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+using Fantasy.BusinessEngine;
+using Fantasy.BusinessEngine.Services;
+
+namespace Fantasy.Web.Mvc.Html
+{
+    public static class BusinessApplicationExtensions
+    {
+        public static MvcHtmlString ApplicationLink(this HtmlHelper htmlHelper, string linkText, string appName, object routeValues)
+        {
+            RouteValueDictionary values2 = new RouteValueDictionary(routeValues);
+            values2.Add("AppName",appName);
+            return htmlHelper.RouteLink(linkText, values2); 
+        }
+
+
+
+        public MvcHtmlString ScalarView(this HtmlHelper helper, Guid objectId)
+        {
+
+             IEntityService es = BusinessEngineContext.Current.GetRequiredService<IEntityService>();
+             BusinessObject obj = es.Get<BusinessObject>(objectId);
+             IScalarViewController controller = BusinessEngineContext.Current.Application.GetScalarView(obj);
+
+             throw new NotImplementedException();
+        }
+    }
+}
