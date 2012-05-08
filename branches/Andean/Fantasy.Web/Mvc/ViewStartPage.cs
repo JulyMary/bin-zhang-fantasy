510c8938822ce67283601e108231b26d8282475f
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-05-08 11:23:48 +0000

    Fantasy [1.0.0.176] -- Use Fantasy.Mvc.WebViewPage as compiled page's base class

---
+++ branches/Andean/Fantasy.Web/Mvc/ViewStartPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fantasy.Web.Mvc
+{
+    public class ViewStartPage : System.Web.Mvc.ViewStartPage
+    {
+        public override void Execute()
+        {
+            this.Render();
+        }
+
+        public virtual void Render()
+        {
+        }
+    }
+}
