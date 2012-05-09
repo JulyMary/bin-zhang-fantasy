bbbd517b1e23578ee5dbf853ad9c60eaccbe0321
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-05-09 04:30:44 +0000

    RazorGenerator [1.3.1.13] -- Generate Partial class -- Use only use mvc view for all classes

--- External.Source/razorgenerator/CommonAssemblyInfo.cs
+++ External.Source/razorgenerator/CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.1.11")]
+[assembly: AssemblyVersion("1.3.1.13")]
 [assembly: AssemblyProduct("RazorGenerator")]
 [assembly: AssemblyCompany("RazorGenerator contributors")]
