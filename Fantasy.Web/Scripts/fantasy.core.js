7d247629e5896f207498b9e3f33e7990525abceb
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-05-11 07:25:38 +0000

    Fantasy [1.0.0.179] -- Changing HtmlAssets for compatible with both HTTP GET and Ajax calling

---
+++ Fantasy.Web/Scripts/fantasy.core.js
@@ -0,0 +1,11 @@
+﻿$.appendScript = function (href) {
+    var exists = false;
+    $("head>script").each(function () {
+        if ($(this).attr("src").toLowerCase() == href.toLowerCase()) {
+            exists = true;
+        }
+    })
+    if (!exists) {
+        $.getScript(href);
+    }
+}
