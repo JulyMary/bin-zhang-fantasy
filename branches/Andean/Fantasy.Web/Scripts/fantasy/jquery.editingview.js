d812f31c0ed9e005e3299a4599cd8ef685da09f8
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-06-01 07:33:42 +0000

    Fantasy [1.0.0.197] -- Added messagebox.js -- fix jstree programmic creating node icon size bug

---
+++ branches/Andean/Fantasy.Web/Scripts/fantasy/jquery.editingview.js
@@ -0,0 +1,78 @@
+﻿(function ($) {
+
+    var methods = {
+        init: function (options) {
+
+            var settings = $.extend({
+                isDirty: false
+            }, options);
+
+            return this.each(function () {
+                var $this = $(this);
+                $this.data("editviewOptions", settings);
+            });
+        },
+
+        save: function () {
+            return this.each(function () {
+                var $this = $(this);
+                var options = $this.data("editviewOptions");
+                if (options.save) {
+                    options.save.apply($this);
+                }
+            });
+        },
+
+        refresh: function () {
+            return this.each(function () {
+                var $this = $(this);
+                var options = $this.data("editviewOptions");
+                if (options.refresh) {
+                    options.refresh.apply($this);
+                }
+            });
+        },
+
+
+
+        isDirty: function (val) {
+
+            var rs = false;
+
+            this.each(function () {
+                var $this = $(this);
+                var options = $this.data("editviewOptions");
+
+                if (val != undefined) {
+
+                    if (options.isDirty != val) {
+                        options.isDirty = val;
+                        $this.trigger("isDirtyChanged.editview", val);
+                    }
+                }
+                rs |= options.isDirty;
+            });
+
+            return rs;
+
+        }
+    };
+
+
+
+
+    $.fn.editview = function (method) {
+
+        // Method calling logic
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.tooltip');
+        }
+
+    };
+
+
+})(jQuery);
---
+++ branches/Andean/Fantasy.Web/Scripts/fantasy/messagebox.js
@@ -0,0 +1,223 @@
+﻿var messageBox = new function () {
+
+    this.buttons = {
+        OK: 0,
+        OKCancel: 1,
+        AbortRetryIgnore: 2,
+        YesNoCancel: 3,
+        YesNo: 4,
+        RetryCancel: 5
+
+    };
+
+    this.icons = {
+        Asterisk: 0x40,
+        Error: 0x10,
+        Exclamation: 0x30,
+        Hand: 0x10,
+        Information: 0x40,
+        None: 0,
+        Stop: 0x10,
+        Warning: 0x30
+    };
+
+    this.defaultButtons = {
+        Button1: 0,
+        Button2: 1,
+        Button3: 2
+    };
+
+    this.dialogResult = {
+        None: 0,
+        OK: 1,
+        Cancel: 2,
+        Abort: 3,
+        Retry: 4,
+        Ignore: 5,
+        Yes: 6,
+        No: 7
+    };
+
+    this.show = function (options) {
+
+
+        if (typeof options == 'string') {
+            options = { text: options };
+        }
+
+        options = $.extend({
+            caption: document.title,
+            buttons: this.buttons.OK,
+            icon: this.icons.None,
+            defaultButtons: this.defaultButtons.Button1
+        }, options);
+
+        var id = "#messageboxHiddenDiv";
+        var div = $(id);
+        if (div.length == 0) {
+            div = $("<div id=\"messageboxHiddenDiv\" title=\"Message\" class=\"messagebox\"><ins></ins><p></p></div>");
+            div.appendTo(document.body);
+        }
+
+
+        var icon=$("ins:first", div);
+        icon.attr("class", "");
+        switch(options.icon)
+        {
+            case 0 :
+                icon.addClass("icon icon-none");
+                break;
+            case 0x10 :
+                icon.addClass("icon icon-error");
+                break;
+            case 0x30 :
+                icon.addClass("icon icon-warnning");
+                break;
+            case 0x40 :
+                icon.addClass("icon icon-information");
+                break;
+        }
+
+        $("p:first", div).html(options.text);
+
+
+       
+
+
+       
+        var buttons;
+        switch (options.buttons) {
+            case 0: //OK
+                buttons = {
+                    "OK" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 1);
+                        }
+                    }
+                };
+                break;
+            case 1: //OKCancel
+                buttons = {
+                    "OK" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 1);
+                        }
+                    },
+                    "Cancel" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 2);
+                        }
+                    }
+                };
+                break;
+            case 2: //AbortRetryIgnore
+             buttons = {
+                    "Abort" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 3);
+                        }
+                    },
+                    "Retry" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 4);
+                        }
+                    },
+                    "Ignore" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 5);
+                        }
+                    }
+                };
+                break;
+            case 3: //YesNoCancel
+             buttons = {
+                    "Yes" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 6);
+                        }
+                    },
+                    "No" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 7);
+                        }
+                    },
+                    "Cancel" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 2);
+                        }
+                    }
+                };
+                break;
+            case 4: //YesNo
+                buttons = {
+                    "Yes" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 6);
+                        }
+                    },
+                    "No" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 7);
+                        }
+                    }
+                };
+                break;
+            case 5: //RetryCancel;
+                buttons = {
+                    "Retry" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 4);
+                        }
+                    },
+                    "Cancel" : function(){
+                        $(this).dialog("close");
+                        if(options.callback)
+                        {
+                            options.callback.apply($(this), 2);
+                        }
+                    }
+                };
+                break; 
+        }
+
+        div.dialog({
+            resizable:false,
+            modal:true,
+            buttons:buttons,
+            width:"auto",
+            title:options.caption
+        });
+
+        var b = $("button", div.closest(".ui-dialog"));
+        if(options.defaultButtons < b.length)
+        {
+            $(b[options.defaultButtons]).focus();
+        }
+    };
+
+   
+}
