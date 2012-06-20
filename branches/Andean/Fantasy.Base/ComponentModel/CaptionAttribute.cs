471fdd55063fa5cd34e5608ee5e88f9c100b813a
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-06-20 07:01:41 +0000

    Fantasy [1.0.0.220] -- Adding ApplicationView Designer

---
+++ branches/Andean/Fantasy.Base/ComponentModel/CaptionAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Fantasy.ComponentModel
+{
+    public class CaptionAttribute : Attribute
+    {
+        public CaptionAttribute(string caption)
+        {
+            this.Caption = caption;
+        }
+
+        public string Caption { get; private set; }
+    }
+
+
+    public class ResourceCaptionAttribute : CaptionAttribute
+    {
+        public ResourceCaptionAttribute(Type type, string key)
+            : base((string)Invoker.InvokeStatic(type, key))
+        {
+           
+            
+        }
+
+        public ResourceCaptionAttribute(string typeName, string key)
+            : this(Type.GetType(typeName, true), key)
+        {
+          
+        }
+    }
+}
