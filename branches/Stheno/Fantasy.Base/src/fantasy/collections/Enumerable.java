c22e3f96301c48b7d3f1cf5fdb2a3d30c1929994
woshizhangbin@gmail.com <woshizhangbin@gmail.com@0721cb5e-c8a2-b4c9-680e-1b606db6f06e>
2012-07-30 15:51:27 +0000

    fantasy optimized enumerable algorithm

--- branches/Stheno/Fantasy.Base/src/fantasy/collections/Enumerable.java
+++ branches/Stheno/Fantasy.Base/src/fantasy/collections/Enumerable.java
@@ -24,22 +24,116 @@ public  class Enumerable<T> implements Iterable<T> {
 			throw new IllegalArgumentException("source");
 		}
 
-		this._source = Arrays.asList(source);
+		this._source = new ArrayIterable(source);
+	}
+	
+	private class ArrayIterable implements Iterable<T>
+	{
+		public ArrayIterable(T[] source)
+		{
+			this._source = source;
+		}
+
+		@Override
+		public Iterator<T> iterator() {
+			
+			return new ArrayIterator(this._source);
+		}
+		
+		private T[] _source;
+		
+	}
+	
+	private class ArrayIterator implements Iterator<T>
+	{
+		public ArrayIterator(T[] source)
+		{
+			this._source = source;
+		}
+		
+		private int _index = 0;
+
+		private T[] _source;
+
+		@Override
+		public boolean hasNext() {
+			return _index < this._source.length;
+		}
+
+		@Override
+		public T next() {
+			if(_index < this._source.length)
+			{
+				T rs = this._source[this._index];
+				this._index ++;
+				return rs;
+			}
+			else
+			{
+				throw new NoSuchElementException();
+			}
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+			
+		}
+		
 	}
 
 	public <TResult> Enumerable<TResult> select(Selector<T, TResult> selector)
 	{
-		ArrayList<TResult> rs = new ArrayList<TResult>();
-		for(T item : _source)
-		{
-
-			rs.add(selector.select(item));
-
-		}
-
+		Iterable<TResult> rs = new SelectIterable<TResult>(this._source, selector);
 		return new Enumerable<TResult>(rs);
 
 	}
+	
+	
+	private class SelectIterable<TResult> implements Iterable<TResult>
+	{
+		public SelectIterable(Iterable<T> source, Selector<T, TResult> selector)
+		{
+			this._source = source;
+			this._selector = selector;
+		}
+		
+		private Iterable<T> _source;
+		private Selector<T, TResult> _selector;
+		@Override
+		public Iterator<TResult> iterator() {
+			return new SelectIterator<TResult>(this._source, this._selector);
+		}
+	}
+	
+	private class SelectIterator<TResult> implements Iterator<TResult>
+	{
+		public SelectIterator(Iterable<T> source, Selector<T, TResult> selector)
+		{
+			this._source = source.iterator();
+			this._selector = selector;
+		}
+		
+		private Iterator<T> _source;
+		private Selector<T, TResult> _selector;
+		@Override
+		public boolean hasNext() {
+			return _source.hasNext();
+		}
+		@Override
+		public TResult next() {
+			T item = this._source.next();
+			TResult rs = this._selector.select(item);
+			return rs;
+		}
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+			
+		}
+		
+	}
+	
 
 
 	public Enumerable<T> distinct()
@@ -74,8 +168,6 @@ public  class Enumerable<T> implements Iterable<T> {
 		private Iterable<T> _source;
 		private Predicate<T> _predicate;
 		
-		
-		private int state = 0;
 
 		@Override
 		public Iterator<T> iterator() {
@@ -213,34 +305,30 @@ public  class Enumerable<T> implements Iterable<T> {
 
 
 
-	@SuppressWarnings("unchecked")
-	public <T2> Enumerable<T2> ofType(Class<T2> clazz)
+	public <T2> Enumerable<T2> ofType(final Class<T2> clazz) throws Exception
 	{
-		ArrayList<T2> rs = new ArrayList<T2>();
-		for(Object item : this._source)
-		{
+		
+		return this.where(new Predicate<T>(){
 
-			if(clazz.isInstance(item))
-			{
-				rs.add((T2)item);
-			}
-		}
-
-		return new Enumerable<T2>(rs);
+			@Override
+			public boolean evaluate(T obj) throws Exception {
+				
+				return clazz.isInstance(obj);
+			}}).cast(clazz);
+		
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <T2> Enumerable<T2> cast(Iterable source)
+	public <T2> Enumerable<T2> cast(Class<T2> t2)
 	{
-		ArrayList<T2> rs = new ArrayList<T2>();
-		for(Object item : source)
-		{
+		
 
-			rs.add((T2)item);
+		return this.select(new Selector<T, T2>(){
 
-		}
-
-		return new Enumerable(rs);
+			@Override
+			public T2 select(T item) {
+				return (T2)item;
+			}});
 
 	}
 
@@ -429,16 +517,92 @@ public  class Enumerable<T> implements Iterable<T> {
 	
 	public <T2> Enumerable<T2> from(Selector<T, Iterable<T2>> selector)
 	{
-		ArrayList<T2> rs = new ArrayList<T2>();
-		for(T item : this._source)
-		{
-			for(T2 item2 : selector.select(item))
-			{
-				rs.add(item2);
-			}
-		}
+		Iterable<T2> rs = new FromIterable<T2>(this._source, selector);
 		
 		return new Enumerable<T2>(rs);
+	}
+	
+	private class FromIterable<T2> implements Iterable<T2>
+	{
+		public FromIterable(Iterable<T> source, Selector<T, Iterable<T2>> selector)
+		{
+			this._source = source;
+			this._selector = selector;
+		}
+		
+		private Iterable<T> _source;
+		private Selector<T, Iterable<T2>> _selector;
+
+		@Override
+		public Iterator<T2> iterator() {
+			return new FromIterator<T2>(this._source, this._selector);
+		}
+	}
+	
+	
+	private class FromIterator<T2> implements Iterator<T2>
+	{
+		public FromIterator(Iterable<T> source, Selector<T, Iterable<T2>> selector)
+		{
+			this._source = source.iterator();
+			this._selector = selector;
+		}
+		
+		private Iterator<T> _source;
+		private Iterator<T2> _currentIterator;
+		private Selector<T, Iterable<T2>> _selector;
+		//0: need to call find next; 1: has item; 2 : end
+		private int _state = 0;
+		private T2 _current;
+		
+		@Override
+		public boolean hasNext() {
+			if(this._state == 0)
+			{
+				this.findNext();
+			}
+			return this._state != 2;
+		}
+		private void findNext() {
+			while((_currentIterator == null || !_currentIterator.hasNext()) && this._state != 2)
+			{
+				if(_source.hasNext())
+				{
+					_currentIterator = _selector.select(_source.next()).iterator();
+				}
+				else
+				{
+					this._state = 2;
+				}
+			}
+			
+			if(this._state != 2)
+			{
+				this._current = this._currentIterator.next();
+			}
+			
+		}
+		@Override
+		public T2 next() {
+			if(_state == 0)
+			{
+				this.findNext();
+			}
+			if(_state == 2)
+			{
+				throw new NoSuchElementException();
+			}
+			T2 rs = this._current;
+			this._state = 0;
+			return rs;
+		}
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+			
+		}
+
+	
 	}
 
 
